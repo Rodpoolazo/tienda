486e1fc7bc9e021b74bb379740e85e89319b02a2
Rodpoolazo <rodpoolazo@gmail.com>
2019-06-17 21:34:57 -0400

    add product y precio en Categoria y otros

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,25 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:destroy]
+
+    def index
+      @products = Product.all
+    end
+
+
+
+    def new
+      @product = Product.new
+    end
+
+    def edit
+    end
 
     def create
       @product = Product.new(product_params)
 
       respond_to do |format|
         if @product.save
-          format.html { redirect_to @product, notice: 'Category was successfully created.' }
+          format.html { redirect_to @product.category, notice: 'Category was successfully created.' }
           format.json { render :show, status: :created, location: @product }
         else
           format.html { render :new }
@@ -17,15 +31,19 @@ class ProductsController < ApplicationController
     def destroy
       @product.destroy
       respond_to do |format|
-        format.html { redirect_to product_url, notice: 'Product was successfully destroyed.' }
+        format.html { redirect_to @product.category, notice: 'Product was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
 
 private
 
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
     def product_params
-      params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:category_id, :name, :price)
     end
 
 
